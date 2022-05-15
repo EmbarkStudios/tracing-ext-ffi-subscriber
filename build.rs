0c58656fec4e66b4657fd3e056521ac3ac574608
Tom Solberg <me@sbg.dev>
2022-05-15 16:14:40 +0200

    more docs

--- build.rs
+++ build.rs
@@ -1,7 +1,13 @@
 fn main() {
     let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    let target_dir = std::env::var("OUT_DIR").unwrap();
-    let target_file = std::path::PathBuf::from(target_dir).join("../../../../libtracing_ffi.h");
+
+    let target_file = match std::env::var("TRACING_FFI_RELATIVE_OUT_PATH") {
+        Ok(relative) => std::path::PathBuf::from(&crate_dir).join(relative),
+        Err(_) => {
+            let target_dir = std::env::var("OUT_DIR").unwrap();
+            std::path::PathBuf::from(target_dir).join("../../../../tracing_ffi.h")
+        }
+    };
 
     cbindgen::generate(crate_dir)
         .expect("Unable to generate bindings")
